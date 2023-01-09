2c844eb2b3ef9a51673592fecca2d24b98f63df0
Tadgh Purcell <tadghp@gmail.com>
2023-01-09 16:54:06 +0000

    Creata function playRound
    This function compares the user input and the computers randomly generated input
    and creates options for all outcomes including if the player mispells an option.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 'use strict'
 const playerSelection = prompt("Pick you choice: Rock, Paper or Scissors");
-const playerChoice = playerSelection.toUpperCase(0);
+const playerChoice = playerSelection.toLowerCase(0);
 
 console.log(playerSelection);
 console.log(playerChoice);
@@ -8,17 +8,42 @@ console.log(playerChoice);
 function getComputerChoice() {
     const computerChoice = Math.floor(Math.random() * 3);
     if (computerChoice === 0) {
-        return "Rock";
+        return "rock";
     } else if (computerChoice === 1) {
-        return "Paper";
+        return "paper";
     } else {
-        return "Scissors";
+        return "scissors";
     }
 }
 
 console.log(getComputerChoice());
 
-const computerSelection = getComputerChoice();
-const playRound = function (playerSelection, computerSelection) {
 
+const playRound = function (playerSelection, computerSelection) {
+    playerSelection = playerChoice;
+    computerSelection = getComputerChoice();
+
+    if (playerSelection === 'rock' && computerSelection === 'rock') {
+        return 'Rock vs. Rock. Tie Game!'
+    } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        return 'You lose! Paper beats Rock.'
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        return 'You win! Rock beats Scissors.'
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        return 'You win! Paper beats Rock.'
+    } else if (playerSelection === 'paper' && computerSelection === 'paper') {
+        return 'Paper vs. Paper. Tie Game!'
+    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        return 'You lose! Scissors beats Paper.'
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        return 'You lose! Rock beats Scissors.'
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        return 'You win! Scissors beats paper.'
+    } else if (playerSelection === 'scissors' && computerSelection === 'scissors') {
+        return 'Scissors vs. Scissors. Tie Game!'
+    } else {
+        return "Invalid Option! Please pick Rock, Paper or Scissors."
+    }
 }
+
+console.log(playRound())
