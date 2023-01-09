95770f426cbbc1533711ffac325741ce90ac776f
Tadgh Purcell <tadghp@gmail.com>
2023-01-09 16:16:49 +0000

    Create function getComputerChoice
    Created function to randomly select 1 of 3 options
    Rock, Paper and Scissors so the computer will
    select one after the users input to see who wins.

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+'use strict'
 let playerSelection = prompt("Pick you choice: Rock, Paper or Scissors");
 
 console.log(playerSelection);
+
+function getComputerChoice() {
+    const computerChoice = Math.floor(Math.random() * 3);
+    if (computerChoice === 0) {
+        return "Rock";
+    } else if (computerChoice === 1) {
+        return "Paper";
+    } else {
+        return "Scissors";
+    }
+}
+
+console.log(getComputerChoice());
