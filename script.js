b02bf1f3733f7fa4848f8b4fedd2afaf181002de
Tadgh Purcell <tadghp@gmail.com>
2023-01-11 11:23:24 +0000

    Fix bugs and add start game button to initialize prompt
    Add start button to invoke buttonClick function to prompt to start game
    Done to solve the problem of the initial prompt preventing the console logs
    to start. Paremeters added to whoWon and playRound function to prevents prompt
    repeating 3 times and arguments added in game function.

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 'use strict'
-let startGame = prompt('Start Game?');
-let playerSelection = prompt("Pick you choice: Rock, Paper or Scissors"); // initial prompt
-let playerChoice = playerSelection.toLowerCase(0); // converts prompt to lower case to make insensitive
+// let playerChoice = prompt("Pick you choice: Rock, Paper or Scissors");
+// let playerSelection = playerChoice.toLowerCase(0);
 let playerScore = 0;
 let computerScore = 0;
 
@@ -26,9 +25,7 @@ console.log(getComputerChoice()); //test function
 const computerSelection = getComputerChoice(); // variable made to use in argument for paremeter
 // function to play game uses all options
 
-function playRound() {
-    // playerSelection // use parameter playerSelection to get results from prompt
-    // computerSelection // use parameter computerSelection to get result from computer
+function playRound(playerSelection, computerSelection) {
 
     if (playerSelection === 'rock' && computerSelection === 'rock') {
         return 'Rock vs. Rock. Tie Game!';
@@ -53,7 +50,7 @@ function playRound() {
     }
 }
 
-function whoWon() {
+function whoWon(playerSelection, computerSelection) {
     if (playerSelection === 'rock' && computerSelection === 'scissors' || playerSelection === 'paper' && computerSelection === 'rock' || playerSelection === 'scissors' && computerSelection === 'paper') {
         playerScore += 1;
     } else if (playerSelection === 'rock' && computerSelection === 'paper' || playerSelection === 'paper' && computerSelection === 'scissors' || playerSelection === 'scissors' && computerSelection === 'rock') {
@@ -71,31 +68,38 @@ console.log(computerScore);
 
 function game() {
     for (playerScore, computerScore; playerScore <= 4, computerScore <= 4;) {
-        playerSelection = prompt("Pick you choice: Rock, Paper or Scissors");
-        playerChoice
-        whoWon();
+        let playerChoice = prompt("Pick you choice: Rock, Paper or Scissors");
+        let playerSelection = playerChoice.toLowerCase(0);
+        playerSelection;
+        whoWon(playerSelection, computerSelection);
         console.log(playerScore);
         console.log(computerScore); // if don't have increments it will never stop increasing
-        console.log(playRound());
+        console.log(playRound(playerSelection, computerSelection));
+        if (playerScore >= 5) {
+            playerScore = 0;
+            computerScore = 0;
+            console.log('Player Wins');
+            return;
+        } else if (computerScore >= 5) {
+            playerScore = 0;
+            computerScore = 0;;
+            console.log('Computer Wins!');
+            return;
+        }
     }
 
 }
 
 //must enter no to work sort tomorrow
-if (startGame === 'yes') {
-    game();
-} else {
-    alert('please start game');
-    game();
+function buttonClick() {
+    let startGame = prompt('Start Game?')
+    if (startGame === 'yes') {
+        alert('Best of luck!');
+        game();
+    } else {
+        alert('You have no choice');
+        game();
+    }
 }
-game();
+// game();
 
-if (playerScore >= 5) {
-    playerScore = 0;
-    computerScore = 0;
-    console.log('Player Wins!')
-} else if (computerScore >= 5) {
-    playerScore = 0;
-    computerScore = 0;;
-    console.log('Computer Wins!')
-}
