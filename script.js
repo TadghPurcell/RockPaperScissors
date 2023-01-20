cd539f0e4186ffb15e151344cef717e4e32f24ae
Tadgh Purcell <tadghp@gmail.com>
2023-01-20 15:12:44 +0000

    Fix game play bugs
    Fix bugs regarding game finishing as soon as score hits 5 and scores updating
    alongside dialog box.

--- script.js
+++ script.js
@@ -44,30 +44,31 @@ function getComputerChoice() {
 
 // function to return string every time round is played depending on result of user/comp input (invoked in game() function)
 function playRound(playerSelection, computerSelection) {
-  //update scores
-  score0El.textContent = playerScore;
-  score1El.textContent = computerScore;
   //
   playerSelection = playerInput;
   computerSelection = getComputerChoice();
-  whoWon(playerSelection, computerSelection);
-  game();
 
   if (playerSelection === 'rock' && computerSelection === 'rock') {
-    dialog.textContent = 'scissors vs. Rock. Tie Game!';
+    dialog.textContent = 'Rock vs. Rock. Tie Game!';
   } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+    computerScore += 1;
     dialog.textContent = 'You lose! Paper beats Rock.';
   } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+    playerScore += 1;
     dialog.textContent = 'You win! Rock beats Scissors.';
   } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+    playerScore += 1;
     dialog.textContent = 'You win! Paper beats Rock.';
   } else if (playerSelection === 'paper' && computerSelection === 'paper') {
     dialog.textContent = 'Paper vs. Paper. Tie Game!';
   } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+    computerScore += 1;
     dialog.textContent = 'You lose! Scissors beats Paper.';
   } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+    computerScore += 1;
     dialog.textContent = 'You lose! Rock beats Scissors.';
   } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+    playerScore += 1;
     dialog.textContent = 'You win! Scissors beats paper.';
   } else if (
     playerSelection === 'scissors' &&
@@ -77,6 +78,10 @@ function playRound(playerSelection, computerSelection) {
   } else {
     dialog.textContent = 'Invalid Option! Please pick Rock, Paper or Scissors.';
   }
+  //update scores
+  score0El.textContent = playerScore;
+  score1El.textContent = computerScore;
+  game();
 }
 
 // function to increase user or computer score by 1 depending on result of comp/user input
@@ -99,19 +104,20 @@ function whoWon(playerSelection, computerSelection) {
 // most important function - runs loop to increment and keep track of scores and executes all other functions as prompt is entered to return a new value for scores, and a string to show the events of the round
 function game() {
   if (playerScore >= 5) {
+    score0El.textContent = 5;
     console.log(`Player Wins! (${playerScore}) vs. (${computerScore}).`);
     winner.textContent = `Player Wins! (${playerScore}) vs. (${computerScore}).`;
-    playerScore = 0;
-    computerScore = 0;
+    // playerScore = 0;
+    // computerScore = 0;
     btnAgain.classList.remove('hidden');
     return;
   } else if (computerScore >= 5) {
+    score1El.textContent = 5;
     console.log(`Computer Wins! (${computerScore}) vs. (${playerScore}).`);
     winner.textContent = `Computer Wins! (${computerScore}) vs. (${playerScore}).`;
-    playerScore = 0;
-    computerScore = 0;
+    // playerScore = 0;
+    // computerScore = 0;
     btnAgain.classList.remove('hidden');
-
     return;
   }
 }
@@ -120,3 +126,13 @@ function game() {
 for (let i = 0; i < btns.length; i++) {
   btns[i].addEventListener('click', playRound);
 }
+
+btnAgain.addEventListener('click', function () {
+  playerScore = 0;
+  computerScore = 0;
+  dialog.textContent = '';
+  winner.textContent = '';
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  btnAgain.classList.toggle('hidden');
+});
